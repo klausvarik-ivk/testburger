1e4423e0995165e19ae705c66b983ff771fa08e5
Kristjan Krimm <kristjan.krimm@vikk.ee>
2021-11-17 23:32:42 +0200

    create sidemenu/add to home.vue

---
+++ src/components/Sidemenu.vue
@@ -0,0 +1,24 @@
+<template>
+ <div class="col-3">
+    <ul>
+      <li>
+        <a href="#" target="_blank" rel="noopener">Söögid</a>
+      </li>
+      <li>
+        <a href="#" target="_blank" rel="noopener"
+          >Joogid</a
+        >
+      </li>
+      <li>
+        <a href="#" target="_blank" rel="noopener"
+          >Kojuvedu</a
+        >
+      </li>
+      <li>
+        <a href="#" target="_blank" rel="noopener"
+          >Kontakt</a
+        >
+      </li>
+    </ul>
+</div>   
+</template>
