2f724bfa9d46bd8087da121dc124e792b6a7f897
annagrrr <90323869+annagrrr@users.noreply.github.com>
2021-11-17 23:50:00 +0200

    add Contacts.vue

---
+++ src/components/Contacts.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <div class="address">
+      <h2>Aadress: Aadress 123, Viljandi</h2>
+    </div>
+    
+    <div class= "telephone">
+      <h2>Telefon: 512 3456</h2>
+    </div>
+
+    <div class= "email">
+      <h2>E-mail: kaburger@gmail.com</h2>
+    </div>
+
+    <div class="open">
+      <h2>Oleme avatud:</h2>
+      <div class="hours">
+    <h3> E-R 10-20</h3>
+    <h3> L-P 10-22</h3>
+      </div>
+    </div>
+  </div 
+</template>
+
+<script>
+
+</script>
+
+
+<style scoped>
+
+</style>
