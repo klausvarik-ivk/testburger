1f84fe33d7f45d5f35ab146c66992c35b0d72868
klausvarik <klaus.varik@vikk.ee>
2021-11-29 13:43:42 +0200

    server nodemon

---
+++ server/dbConnection.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+// Connecting to database
+mongoose.connect(
+  "mongodb+srv://klaus:NTt6_6fqUwU4kBz@cluster0.mwojn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
+const Schema = mongoose.Schema;
+
+const ServSchema = new Schema({
+  title: { type: String },
+  price: { type: Number },
+  stock: { type: Number },
+  img: { type: String },
+});
+
+const Serv = mongoose.model("Kaburger-BE", ServSchema);
+ 
+module.exports = {
+  Serv,
+};
---
+++ server/router.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+const { Serv } = require("./dbConnection");
+
+router.get("/get-burgers", async function (request, response) {
+  const result = await Serv.find();
+  console.log(result);
+  response.send(result);
+});
+
+
+// request === req ja response === res
+/* router.get("/get-todos", async function (request, response) {
+  const result = await Todos.find();
+  console.log(result);
+  response.send(result);
+});
+
+router.get("/delete-todo/:id", async function (request, response) {
+  await Todos.deleteOne({ _id: request.params.id });
+  console.log("Delete todo..");
+  response.send({});
+});
+
+router.get("/get-todo/:id", async function (request, response) {
+  const result = await Todos.findOne({ _id: request.params.id });
+  console.log(result);
+  response.send(result);
+});
+
+router.post("/update-todo/:id", async function (request, response){
+  await Todos.updateOne(
+    { _id: request.params.id },
+    { $set: { status: request.body.status } }
+  );
+  console.log("updating todo..");
+  response.send({});
+});
+*/
+router.post("/add-burgers", async function (request, response) {
+  if (request.body.title) {
+    await Serv.create(request.body);
+    console.log("Lisa burger");
+  }
+  response.send("done");
+}); 
+
+module.exports = router;
