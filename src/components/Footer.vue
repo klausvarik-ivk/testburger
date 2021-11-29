34e319af59346842b3ee3b9d3f878e31f027baa8
timm-u <kristjan.krimm@vikk.ee>
2021-11-29 13:19:12 +0200

    changed a lot of stuff

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="tulge jälle">
-    <h2>Külastage meid jälle</h2>
+  <div class="row">
+    <div class="text-center">
+      Tel: 512 4564 * Aadress: 123 Viljandi * Oleme avatud: E-R 10-20 L-P 10-22
+    </div>
+  </div>
+  <div class="row">
+    <div class="text-center">
+      <h3>Külastage meid jälle!</h3>
+    </div>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,47 +1,9 @@
 <template>
-  <div>
+  <div class="container">
+    <div class="row">
+      <Navbar />
+    </div>
     <h1 class="text-center"><img src="../../img/ka-burx.jpg" alt="logo" /></h1>
-    <header class="header">
-      <nav class="navbar navbar-style">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div class="container-fluid">
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#"
-                    >Home</a
-                  >
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Drinks</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Food</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Home Delivery</a>
-                </li>
-
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Contact</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </nav>
-    </header>
 
     <from id="newsletter">
       <div class="container">
@@ -91,36 +53,23 @@
         </div>
       </div>
     </div>
-    <section id="boxes"></section>
-    <!-- <footer>
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <li class="col">Tel: 512 4564</li>
-        <div class="col">
-          <li class="col">Aadress: 123 Viljandi
-          </div>
-          <div class="col order-first">
-            <ul class="group">
-              <li class="group-item">Oleme Avatud</li>
-              <li class="group-item">E-R 10-20</li>
-              <li class="group-item">L-P 10-22</li>
-            </ul> 
-          </div>
-        </div>
-      </div>      
-</footer> -->
+    <section id="boxes">
+      <Footer /> 
+    </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 //import HelloWorld from "@/components/HelloWorld.vue";
+import Footer from "@/components/Footer.vue";
+import Navbar from "@/components/Navbar.vue";
 
 export default {
   name: "Home",
   components: {
-    //HelloWorld,
+    Footer, 
+    Navbar,
   },
 };
 </script>
