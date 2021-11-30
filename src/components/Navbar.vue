36e34614f98136834f81108587371bf8248ce784
timm-u <kristjan.krimm@vikk.ee>
2021-11-30 10:41:23 +0200

    get code talking with server

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,143 +1,229 @@
 <template>
   <div class="row">
-        <div class="col">
-      <a href="#" data-bs-toggle="modal" data-bs-target="#kanalihaburgerid" @click="getBurgers(burger._id)">
-        <img class="menu-image" src="../../img/sale2.jpg" />
+    <div class="col">
+      <a
+        href="#"
+        data-bs-toggle="modal"
+        data-bs-target="#kanalihaburgerid"
+        @click="getBurgers"
+      >
+        <img class="menu-image" src="../../img/sale2.jpg" @click="getBurgers"/>
         <p>Kanaliha burgerid</p>
         <ul class="list-group mb-3">
           <li
             v-for="burgers in BurgersFromServer"
-            :key="burger"
+            :key="burgers"
             class="list-group-item"
           >
             {{ burger.title }} {{ burger.price }}
           </li>
         </ul>
       </a>
-<!-- Modal -->
-<div class="modal fade" id="kanalihaburgerid" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Kanaliha burgerid
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+      <!-- Modal -->
+      <div
+        class="modal fade"
+        id="kanalihaburgerid"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">Kanaliha burgerid</div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary" @click="getBurgers">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-
-      </div>
-      <div class="col">
+    <div class="col">
       <a href="#" data-bs-toggle="modal" data-bs-target="#veiselihaburgerid">
         <img class="menu-image" src="../../img/sale1.jpg" />
         <p>Veiseliha burgerid</p>
       </a>
 
-<!-- Modal -->
-<div class="modal fade" id="veiselihaburgerid" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Veiseliha burgerid
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+      <!-- Modal -->
+      <div
+        class="modal fade"
+        id="veiselihaburgerid"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">Veiseliha burgerid</div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-
-      </div>
-      <div class="col">
-      <a href="#" data-bs-toggle="modal" data-bs-target="#kalaburgerid" >
+    <div class="col">
+      <a href="#" data-bs-toggle="modal" data-bs-target="#kalaburgerid">
         <img class="menu-image" src="#" alt="" />
-      <p>Kala burgerid</p>
+        <p>Kala burgerid</p>
       </a>
 
-<!-- Modal -->
-<div class="modal fade" id="kalaburgerid" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Kala burgerid
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+      <!-- Modal -->
+      <div
+        class="modal fade"
+        id="kalaburgerid"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">Kala burgerid</div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-
-    </div>
-          <div class="col">
-      <a href="#"  data-bs-toggle="modal" data-bs-target="#veganburgerid" >
+    <div class="col">
+      <a href="#" data-bs-toggle="modal" data-bs-target="#veganburgerid">
         <img class="menu-image" src="../../img/sale3.jpg" alt="" />
-      <p>Vegan burgerid</p>
+        <p>Vegan burgerid</p>
       </a>
 
-<!-- Modal -->
-<div class="modal fade" id="veganburgerid" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-       Vegan burgerid
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+      <!-- Modal -->
+      <div
+        class="modal fade"
+        id="veganburgerid"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">Vegan burgerid</div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-
-    </div>
-          <div class="col">
+    <div class="col">
       <a href="#" data-bs-toggle="modal" data-bs-target="#sealihaburgerid">
         <img class="menu-image" src="../../img/sale4.jpg" alt="" />
-      <p>Sealiha burgerid</p>
+        <p>Sealiha burgerid</p>
       </a>
 
-<!-- Modal -->
-<div class="modal fade" id="sealihaburgerid" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-          Sealiha burgerid
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+      <!-- Modal -->
+      <div
+        class="modal fade"
+        id="sealihaburgerid"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">Sealiha burgerid</div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-</div>
-    </div>
-    </div>
 </template>
 
 <script>
@@ -151,25 +237,22 @@ export default {
     price: Number,
   },
 
-setup() {
+  setup() {
     const BurgersFromServer = ref([]);
 
     async function getBurgers() {
       const result = await axios.get("/api/get-burgers");
       BurgersFromServer.value = result.data;
-      console.log(result.data);
+      console.log(BurgersFromServer);
     }
 
-    getBurgers();
-
-return {
-    getBurgers,
-    BurgersFromServer,
-};
-},
+    return {
+      getBurgers,
+      BurgersFromServer,
+    };
+  },
 };
 </script>
-
 
 <style scoped>
 .menu-image {
