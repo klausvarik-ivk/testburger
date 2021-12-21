79e18b2b860d4c9cb1c80d6659ea17ea8a4be461
Klaus Varik <klaus.varik@vikk.ee>
2021-12-21 09:24:11 +0200

    DB

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -1,23 +1,13 @@
 const mongoose = require("mongoose");
-
+const { Products } = require("./models/ProductSchema");
+const { Users } = require("./models/UsersSchema");
 // Connecting to database
 mongoose.connect(
   "mongodb+srv://klaus:NTt6_6fqUwU4kBz@cluster0.mwojn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   { useNewUrlParser: true }
 );
-const Schema = mongoose.Schema;
 
-const ServSchema = new Schema({
-  title: { type: String },
-  category: {type: String},
-  price: { type: Number },
-  stock: { type: Number },
-  img: { type: String },
-  featured: { type: Boolean},
-});
-
-const Serv = mongoose.model("Kaburger-BE", ServSchema);
- 
 module.exports = {
-  Serv,
+  Products,
+  Users,
 };
