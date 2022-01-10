986de3b8549c78b096bff3be593f41b03af5a4a8
annagrrr <90323869+annagrrr@users.noreply.github.com>
2022-01-10 23:47:27 +0200

    add Admin.vue

---
+++ src/views/Admin.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="d-flex justify-content-center">
+        <div class="col-6">
+           
+            <label class="input-label">Burgeri nimi</label>
+                <input class="form-control">
+        </div>
+    </div>
+    <div class="d-flex justify-content-center">
+        <div class="col-6">
+            <label class="input-label">Kategooria</label>
+                <input class="form-control">
+        </div>
+    </div>
+    <div class="d-flex justify-content-center">
+        <div class="col-6">
+            <label class="input-label">Hind</label>
+                <input class="form-control">
+        </div>
+    </div>
+    <div class="d-flex justify-content-center">
+        <div class="col-6">
+            <label class="input-label">Laoseis</label>
+                <input class="form-control">
+        </div>
+    </div>
+    <div class="d-flex justify-content-center">
+        <div class="col-6">
+            <label class="input-label">Pilt</label>
+                <input type="file" @change="onFileSelected"> 
+            <button @click="onUpload"> Lae üles</button>
+             
+        </div>
+    </div>
+    <div class="d-flex justify-content-center">
+        <div class="col-6">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                        Featured
+                    </label>
+            </div>
+        </div>
+    </div>
+    <div class="d-flex justify-content-center">
+        <div class="col-6">
+            <button type="submit" class="btn btn-primary">Lisa</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+.centered-input input {
+  text-align: center
+}
+</style>
+
+<script>
+import axios from "axios";
+export default {
+    name: "Admin",
+    data() {
+        return {
+            selectedFile: null
+        }
+    },
+    methods: {
+        onFileSelected(event) {
+            this.selectedFile = event.target.files[0]
+        },
+        onUpload() {
+            axios.post("")
+        }
+    }
+}
+
+</script>
