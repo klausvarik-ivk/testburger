2e532bdca65c00b0e1fbdfe12e99e5e29e002704
annagrrr <90323869+annagrrr@users.noreply.github.com>
2022-01-10 23:45:18 +0200

    add /admin path

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,51 @@ const routes = [
   {
     path: "/contacts",
     name: "Kontaktid",
+    component: () =>
+      import("../views/Contacts.vue"),
+  },
+  {
+    path: "/Login",
+    name: "Login",
+    component: () =>
+    import("../views/Login.vue"),
+  },
+  {
+    path: "/Admin",
+    name: "Admin",
+    component: () =>
+    import("../views/Admin.vue"),
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
+
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/contacts",
+    name: "Kontaktid",
     component: () => 
       import(/* webpackChunkName: "about" */ "../views/Contacts.vue"),
   },
@@ -34,6 +79,12 @@ const routes = [
     component: () => 
       import(/* webpackChunkName: "about" */ "../views/Register.vue"),
   },
+  {
+    path: "/Admin",
+    name: "Admin",
+    component: () =>
+    import("../views/Admin.vue"),
+  }
 ];
 
 const router = createRouter({
