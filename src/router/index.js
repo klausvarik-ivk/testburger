71edc7d41a186cb6bbdc3c1aed99b8b28abd26f8
annagrrr <90323869+annagrrr@users.noreply.github.com>
2022-01-11 00:43:24 +0200

    add admin path

--- src/router/index.js
+++ src/router/index.js
@@ -19,51 +19,6 @@ const routes = [
   {
     path: "/contacts",
     name: "Kontaktid",
-    component: () =>
-      import("../views/Contacts.vue"),
-  },
-  {
-    path: "/Login",
-    name: "Login",
-    component: () =>
-    import("../views/Login.vue"),
-  },
-  {
-    path: "/Admin",
-    name: "Admin",
-    component: () =>
-    import("../views/Admin.vue"),
-  }
-];
-
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
-
-export default router;
-
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
-
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-  {
-    path: "/contacts",
-    name: "Kontaktid",
     component: () => 
       import(/* webpackChunkName: "about" */ "../views/Contacts.vue"),
   },
