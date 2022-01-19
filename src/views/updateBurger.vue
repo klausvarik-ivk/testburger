b3eabaa1f38b58bc86293e82263033bd965c9a89
annagrrr <90323869+annagrrr@users.noreply.github.com>
2022-01-19 14:40:22 +0200

    fix updateBurger

--- src/views/updateBurger.vue
+++ src/views/updateBurger.vue
@@ -1,25 +1,287 @@
 <template>
-
-<div class= "row burger-list"> 
-
-          <div class="col-mb-3"
-            v-for="burgers in BurgersromServer"
-            :key="burgers"
-            >
-            <p> 
-            {{ burgers.title }} {{ burgers.category}} {{ burgers.price }}€ {{ burgers.stock }} <img :src=" burgers.img " {{ burgers.featured }} />
-            </p>
-           </div>
+<div>
+<a href="/admin">Lisa burgereid</a>
 </div>
+<div class="col">
+      <a href="#" data-bs-toggle="modal" data-bs-target="#sealihaburgerid" @click="getBurgers('Sea')">
+        <p>Sealiha burgerid</p>
+      </a>
+      
+ <div
+        class="modal fade"
+        id="sealihaburgerid"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              Sealiha burgerid
+                                         
+                                         
+       <ul class="list-group mb-3">
+          <li
+            v-for="burgers in BurgersFromServer"
+            :key="burgers"
+            class="list-group-item"
+          >
+            {{ burgers.title }} {{ burgers.price }}€ {{ burgers.stock }} in stock image url: <img :src=" burgers.img  " />
+            <button @click="deleteBurger(burgers._id, 'Sea')">Delete</button>
+          </li>
+        </ul>
+              </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+ <div class="col">
+      <a
+        href="#"
+        data-bs-toggle="modal"
+        data-bs-target="#kanalihaburgerid"
+        @click="getBurgers('Kana')"
+      >
+        <p>Kanaliha burgerid</p>
+      </a>
+<div
+        class="modal fade"
+        id="kanalihaburgerid"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              Kanaliha burgerid
+<ul class="list-group mb-3">
+          <li
+            v-for="burgers in BurgersFromServer"
+            :key="burgers"
+            class="list-group-item"
+          >
+            {{ burgers.title }} {{ burgers.price }}€ {{ burgers.stock }} in stock image url: <img :src=" burgers.img  " />
+            <button @click="deleteBurger(burgers._id, 'Kana')">Delete</button>
+          </li>
+        </ul>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <!-- <button type="button" class="btn btn-primary" @click="getBurgers('Kana')"> -->
+                <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>  
+
+<div class="col">
+      <a href="#" data-bs-toggle="modal" data-bs-target="#veiselihaburgerid" @click="getBurgers('Veise')">
+        <p>Veiseliha burgerid</p>
+      </a>
+      <div
+        class="modal fade"
+        id="veiselihaburgerid"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              Veiseliha burgerid
+              <ul class="list-group mb-3">
+          <li
+            v-for="burgers in BurgersFromServer"
+            :key="burgers"
+            class="list-group-item"
+          >
+            {{ burgers.title }} {{ burgers.price }}€ {{ burgers.stock }} in stock image url: <img :src=" burgers.img  " />
+            <button @click="deleteBurger(burgers._id, 'Veise')">Delete</button>
+          </li>
+        </ul>
+              </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"              
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+       <div class="col">
+      <a href="#" data-bs-toggle="modal" data-bs-target="#kalaburgerid" @click="getBurgers('Kala')">
+        <p>Kala burgerid</p>
+      </a>
+<div
+        class="modal fade"
+        id="kalaburgerid"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              Kala burgerid
+                            <ul class="list-group mb-3">
+          <li
+            v-for="burgers in BurgersFromServer"
+            :key="burgers"
+            class="list-group-item"
+          >
+            {{ burgers.title }} {{ burgers.price }}€ {{ burgers.stock }} in stock image url: <img :src=" burgers.img  " />
+            <button @click="deleteBurger(burgers._id, 'Kala')">Delete</button>
+          </li>
+        </ul>
+              </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> 
+
+<div class="col">
+      <a href="#" data-bs-toggle="modal" data-bs-target="#veganburgerid" @click="getBurgers('Vegan')">
+        <p>Vegan burgerid</p>
+      </a>
+<div
+        class="modal fade"
+        id="veganburgerid"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              Vegan burgerid
+                            <ul class="list-group mb-3">
+          <li
+            v-for="burgers in BurgersFromServer"
+            :key="burgers"
+            class="list-group-item"
+          >
+            {{ burgers.title }} {{ burgers.category }} {{ burgers.price }}€ {{ burgers.stock }} {{ burgers.img }} {{ burgers.featured}} />
+            <button @click="updateBurger(burgers._id, 'Vegan')">Update</button>
+          </li>
+        </ul>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
 </template>
 
-<script>
+
+<script> 
 
 import { ref } from "vue";
 import axios from "axios";
 
 export default {
- name: "burgers",
+  name: "burgers",
   props: {
     title: String,
     category: String,
@@ -27,29 +289,35 @@ export default {
     stock: String,
     img: String,
     featured: Boolean,
-  },
+  }, 
 
-  setup(){
-    
-
-      const BurgersFromServer = ref([]);
-    async function getBurgersForUpdate() {
-      const result = await axios.get("/api/update-burgers");
-      BurgersFromServer.value = result.data.filter(result => result.featured === true);
-      console.log(BurgersFromServer.value)
+setup() {
+    const BurgersFromServer = ref([]);
+    async function getBurgers(catburger) {
+      const result = await axios.get("/api/get-burgers");
+      BurgersFromServer.value = result.data.filter(result => result.category === catburger);
+      console.log(BurgersFromServer);
     }
 
-getBurgersForUpdate();
+      const FeaturedFromServer = ref([]);
+    async function getFeatured() {
+      const result = await axios.get("/api/get-burgers");
+      FeaturedFromServer.value = result.data.filter(result => result.featured === true);
+      console.log(FeaturedFromServer.value)
+    }
 
-  return {
 
-getBurgersForUpdate,
-BurgersFromServer,
-
-  };
-
+      async function updateBurger(id, catburger) {
+      await axios.get("api/update-burger/" + id);
+      updateBurger(catburger);
+    }
+    return {
+      getBurgers,
+      BurgersFromServer,
+      getFeatured,
+      FeaturedFromServer,
+      updateBurger,
+    };
   },
-
 };
-
-  </script>
+</script>
